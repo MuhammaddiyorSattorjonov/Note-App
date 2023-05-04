7db65206e232f97d0cfb11703c6c492b6b324cb6
mr_as <sattorjonovmuhammaddiyor@gmail.com>
2023-05-04 13:26:26 +0500

    Note App

---
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.noteapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val home = HomeFragment()
+        replaceFragment(HomeFragment.newInstance(),false)
+
+    }
+    private fun replaceFragment(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.add(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val fragments = supportFragmentManager.fragments
+        if (fragments.size == 0){
+            finish()
+        }
+    }
+}
